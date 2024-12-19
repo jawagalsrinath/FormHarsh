90da4f716746b77e1228711e7b48fd0a9e5fb933
Harshithaa RG <119210947+HarshithaaRG@users.noreply.github.com>
2024-12-19 21:31:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express=require('express')
 const mysql=require('mysql2')
-const cors=require('cors')
 const bodyParser=require('body-parser')
 require('dotenv').config();
 
 
 const app=express()
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://employee-form-frontend-1ebl.onrender.com' }));
+
 
 const db=mysql.createConnection({
     user:process.env.DB_USER,
