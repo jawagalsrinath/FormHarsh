3b8a08aa2cccfda8fec9d2d0003705575e751dbf
Harshithaa RG <119210947+HarshithaaRG@users.noreply.github.com>
2024-12-19 21:47:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const mysql=require('mysql2')
 const bodyParser=require('body-parser')
+const cors=require('cors')
 require('dotenv').config();
 
 
 const app=express()
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 
 
